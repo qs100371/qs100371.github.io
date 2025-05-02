c40889e9fbf4e4144aef1f6b09829a053a2c06ec
qs100371 <ease_lee@163.com>
2025-05-02 20:47:29 +0800

    Site updated: 2025-05-02 20:47:27

--- js/schedule.js
+++ js/schedule.js
@@ -1,9 +1,138 @@
-(()=>{let m={orderBy:"startTime",showLocation:!1,offsetMax:72,offsetMin:4,showDeleted:!1,singleEvents:!0,maxResults:250},v=(Object.assign(m,CONFIG.calendar),new Date);var e=new Date,t=new Date,e=(e.setHours(v.getHours()+m.offsetMax),t.setHours(v.getHours()-m.offsetMin),{key:m.api_key,orderBy:m.orderBy,timeMax:e.toISOString(),timeMin:t.toISOString(),showDeleted:m.showDeleted,singleEvents:m.singleEvents,maxResults:m.maxResults});let n=new URL(`https://www.googleapis.com/calendar/v3/calendars/${m.calendar_id}/events`);function i(e,t,n,a){var s,o,r,i,l={weekday:"short",hour:"2-digit",minute:"2-digit"},d="now"===e?"NOW":(s=v,r=30*(c=24*(d=36e5)),i=365*c,o=0<(s=s-(o=n))?" ago":" later",(s=Math.abs(s))<d?Math.round(s/6e4)+" minutes"+o:s<c?Math.round(s/d)+" hours"+o:s<r?"about "+Math.round(s/c)+" days"+o:s<i?"about "+Math.round(s/r)+" months"+o:"about "+Math.round(s/i)+" years"+o),c=n.toLocaleTimeString([],l)+" - "+a.toLocaleTimeString([],l);let u="",h=(m.showLocation&&t.location&&(u=`<span class="event-location event-details">${t.location}</span>`),"");return t.description&&(h=`<span class="event-description event-details">${t.description}</span>`),`<section class="event event-${e}">
+/* global CONFIG */
+
+// https://developers.google.com/calendar/api/v3/reference/events/list
+(function() {
+  // Initialization
+  const calendar = {
+    orderBy     : 'startTime',
+    showLocation: false,
+    offsetMax   : 72,
+    offsetMin   : 4,
+    showDeleted : false,
+    singleEvents: true,
+    maxResults  : 250
+  };
+
+  // Read config form theme config file
+  Object.assign(calendar, CONFIG.calendar);
+
+  const now = new Date();
+  const timeMax = new Date();
+  const timeMin = new Date();
+
+  timeMax.setHours(now.getHours() + calendar.offsetMax);
+  timeMin.setHours(now.getHours() - calendar.offsetMin);
+
+  // Build URL
+  const params = {
+    key         : calendar.api_key,
+    orderBy     : calendar.orderBy,
+    timeMax     : timeMax.toISOString(),
+    timeMin     : timeMin.toISOString(),
+    showDeleted : calendar.showDeleted,
+    singleEvents: calendar.singleEvents,
+    maxResults  : calendar.maxResults
+  };
+
+  const request_url = new URL(`https://www.googleapis.com/calendar/v3/calendars/${calendar.calendar_id}/events`);
+  Object.entries(params).forEach(param => request_url.searchParams.append(...param));
+
+  function getRelativeTime(current, previous) {
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30;
+    const msPerYear = msPerDay * 365;
+
+    let elapsed = current - previous;
+    const tense = elapsed > 0 ? ' ago' : ' later';
+
+    elapsed = Math.abs(elapsed);
+
+    if (elapsed < msPerHour) {
+      return Math.round(elapsed / msPerMinute) + ' minutes' + tense;
+    } else if (elapsed < msPerDay) {
+      return Math.round(elapsed / msPerHour) + ' hours' + tense;
+    } else if (elapsed < msPerMonth) {
+      return 'about ' + Math.round(elapsed / msPerDay) + ' days' + tense;
+    } else if (elapsed < msPerYear) {
+      return 'about ' + Math.round(elapsed / msPerMonth) + ' months' + tense;
+    }
+
+    return 'about ' + Math.round(elapsed / msPerYear) + ' years' + tense;
+  }
+
+  function buildEventDOM(tense, event, start, end) {
+    const durationFormat = {
+      weekday: 'short',
+      hour   : '2-digit',
+      minute : '2-digit'
+    };
+    const relativeTime = tense === 'now' ? 'NOW' : getRelativeTime(now, start);
+    const duration = start.toLocaleTimeString([], durationFormat) + ' - ' + end.toLocaleTimeString([], durationFormat);
+
+    let location = '';
+    if (calendar.showLocation && event.location) {
+      location = `<span class="event-location event-details">${event.location}</span>`;
+    }
+    let description = '';
+    if (event.description) {
+      description = `<span class="event-description event-details">${event.description}</span>`;
+    }
+
+    const eventContent = `<section class="event event-${tense}">
         <h2 class="event-summary">
-          ${t.summary}
-          <span class="event-relative-time">${d}</span>
+          ${event.summary}
+          <span class="event-relative-time">${relativeTime}</span>
         </h2>
-        ${u}
-        <span class="event-duration event-details">${c}</span>
-        ${h}
-      </section>`}function a(){let r=document.querySelector(".event-list");r&&fetch(n.href).then(e=>e.json()).then(e=>{if(0===e.items.length)r.innerHTML="<hr>";else{r.innerHTML="";let s=0,o=6e4*(new Date).getTimezoneOffset();e.items.forEach(e=>{var t=new Date(e.start.dateTime||new Date(e.start.date).getTime()+o),n=new Date(e.end.dateTime||new Date(e.end.date).getTime()+o);let a="now";n<v?a="past":t>v&&(a="future"),"future"===a&&s<v&&r.insertAdjacentHTML("beforeend","<hr>"),r.insertAdjacentHTML("beforeend",i(a,e,t,n)),s=n})}})}Object.entries(e).forEach(e=>n.searchParams.append(...e)),a();let s=setInterval(a,6e4);document.addEventListener("pjax:send",()=>{clearInterval(s)})})();
+        ${location}
+        <span class="event-duration event-details">${duration}</span>
+        ${description}
+      </section>`;
+    return eventContent;
+  }
+
+  function fetchData() {
+    const eventList = document.querySelector('.event-list');
+    if (!eventList) return;
+
+    fetch(request_url.href).then(response => {
+      return response.json();
+    }).then(data => {
+      if (data.items.length === 0) {
+        eventList.innerHTML = '<hr>';
+        return;
+      }
+      // Clean the event list
+      eventList.innerHTML = '';
+      let prevEnd = 0; // used to decide where to insert an <hr>
+      const utc = new Date().getTimezoneOffset() * 60000;
+
+      data.items.forEach(event => {
+        // Parse data
+        const start = new Date(event.start.dateTime || (new Date(event.start.date).getTime() + utc));
+        const end = new Date(event.end.dateTime || (new Date(event.end.date).getTime() + utc));
+
+        let tense = 'now';
+        if (end < now) {
+          tense = 'past';
+        } else if (start > now) {
+          tense = 'future';
+        }
+
+        if (tense === 'future' && prevEnd < now) {
+          eventList.insertAdjacentHTML('beforeend', '<hr>');
+        }
+
+        eventList.insertAdjacentHTML('beforeend', buildEventDOM(tense, event, start, end));
+        prevEnd = end;
+      });
+    });
+  }
+
+  fetchData();
+  const fetchDataTimer = setInterval(fetchData, 60000);
+  document.addEventListener('pjax:send', () => {
+    clearInterval(fetchDataTimer);
+  });
+})();
--- js/third-party/chat/chatra.js
+++ js/third-party/chat/chatra.js
@@ -1,1 +1,19 @@
-(()=>{CONFIG.chatra.embed&&(window.ChatraSetup={mode:"frame",injectTo:CONFIG.chatra.embed}),window.ChatraID=CONFIG.chatra.id;var a=document.querySelector(".sidebar-button button");a&&a.addEventListener("click",()=>{Chatra("openChat",!0)})})();
+/* global CONFIG, Chatra */
+
+(function() {
+  if (CONFIG.chatra.embed) {
+    window.ChatraSetup = {
+      mode    : 'frame',
+      injectTo: CONFIG.chatra.embed
+    };
+  }
+
+  window.ChatraID = CONFIG.chatra.id;
+
+  const chatButton = document.querySelector('.sidebar-button button');
+  if (chatButton) {
+    chatButton.addEventListener('click', () => {
+      Chatra('openChat', true);
+    });
+  }
+})();
